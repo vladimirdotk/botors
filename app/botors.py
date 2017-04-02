c57f1e7bc9fa9eecee60eb453c4c6de0a36018a1
Vladimir Kivernik <vladimir@kivernik.ru>
2017-04-02 23:08:20 +0500

    Add method to get a note by id. Small refactoring.

--- app/botors.py
+++ app/botors.py
@@ -6,6 +6,7 @@ from flask_bcrypt import Bcrypt
 import config as config
 from response import jsonify
 from functools import wraps
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 mongo = PyMongo(app)
@@ -22,10 +23,11 @@ def login_required(fn):
     """
     @wraps(fn)
     def wrapper(*args, **kwargs):
-        token = request.headers.get('token')
+        token = get_token_from_request(request)
         if token and mongo.db.tokens.find_one({'token': token}):
             return fn(*args, **kwargs)
         abort(401)
+
     return wrapper
 
 
@@ -46,13 +48,36 @@ def login():
     return jsonify({'msg': 'Bad username or password!'}), 401
 
 
+@app.route('/notes/<note_id>', methods=['GET'])
+@login_required
+def get_notes(note_id):
+    """
+    Returns user's note
+    :param str note_id: 
+    :return: 
+    """
+    data = mongo.db.notes.find_one({
+        '_id': ObjectId(note_id),
+        'user_id': get_user_id_by_request(request)
+    }, {'user_id': 0})
+
+    return jsonify(data) if data else jsonify({'msg': 'Not found'}), 404
+
+
 @app.route('/notes', methods=['POST'])
 @login_required
 def add_note():
+    """
+    Adds note to db
+    :return: 
+    """
     json_request = request.get_json()
     if json_request and set(json_request.keys()).issubset(set(NOTE_FIELDS)):
-        data = mongo.db.notes.insert_one(json_request)
-        return jsonify({'id': data.inserted_id}), 201
+        data = mongo.db.notes.insert_one({
+            **json_request,
+            **{'user_id': get_user_id_by_request(request)}
+        })
+        return jsonify({'_id': data.inserted_id}), 201
 
     return jsonify({'msg': 'Bad request'}), 400
 
@@ -63,6 +88,7 @@ def get_user_data(username):
     :param username: 
     :return dict: 
     """
+
     return mongo.db.users.find_one({'username': username})
 
 
@@ -73,7 +99,6 @@ def check_password(username, password):
     :param str password: 
     :return bool: 
     """
-
     data = mongo.db.users.find_one({
         'username': username
     })
@@ -89,7 +114,40 @@ def get_new_token(user_id):
     """
     token = binascii.hexlify(os.urandom(24)).decode()
     mongo.db.tokens.insert_one({'user_id': user_id, 'token': token})
+
     return token
+
+
+def get_user_id_by_request(http_request):
+    """
+    Returns user_id by request
+    :param Request http_request: 
+    :return: 
+    """
+    token = get_token_from_request(http_request)
+
+    return get_user_id_by_token(token)
+
+
+def get_user_id_by_token(token):
+    """
+    Returns user_id by token
+    :param str token: 
+    :return str|None: 
+    """
+    data = mongo.db.tokens.find_one({'token': token})
+
+    return data.get('user_id') if data else None
+
+
+def get_token_from_request(http_request):
+    """
+    Returns token from request
+    :param Request http_request: 
+    :return str: 
+    """
+
+    return http_request.headers.get('token')
 
 
 if __name__ == '__main__':
